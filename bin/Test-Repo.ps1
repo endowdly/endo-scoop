f012b31cb71763208422046f1b9c35966dc9eafd
endowdly <endowdly@gmail.com>
2022-02-02 18:04:57 -0500

    update psparklines

--- bin/Test-Repo.ps1
+++
@@ -1,8 +0,0 @@
-<#
-.Synopsis
-    Execute Pester tests in repository root directory. #>
-
-Invoke-Pester "$PSScriptRoot\..\test" -PassThru | 
-    Set-Variable result
-
-return $result
--- test/Bucket.Tests.ps1
+++
@@ -1,5 +0,0 @@
-if (!($env:SCOOP_HOME)) {
-    $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop)
-}
-
-. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
